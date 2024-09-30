711aee6775da2937d3bdda82bf58b7bd470fb178
spde3289 <spde3289@naver.com>
2024-09-30 14:26:17 +0900

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
-    unoptimized: true,
+    unoptimized: false,
   },
 };
 
